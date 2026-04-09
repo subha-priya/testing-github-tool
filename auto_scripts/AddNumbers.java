d4e2d41a2ae53c70fbd0b00450672bae5af6cf05
subha-priya <subhapriya.pradhan@ascendion.com>
2026-04-09 16:34:16 +0530

    Update AddNumbers.java

--- auto_scripts/AddNumbers.java
+++ auto_scripts/AddNumbers.java
@@ -1,8 +1,18 @@
 public class AddNumbers {
-    public int add(int a, int b) {
-        return a + b;
-    }
 
+//     public class AddNumbers {
+//     public int add(int a, int b) {
+//         return a + b;
+//     }
+
+//     // Method to reverse a string
+//     public String reverseString(String input) {
+//         return new StringBuilder(input).reverse().toString();
+//     }
+// }
+    public int add(int a,int b) {
+        return (a + b);
+    }
     // Method to reverse a string
     public String reverseString(String input) {
         return new StringBuilder(input).reverse().toString();
